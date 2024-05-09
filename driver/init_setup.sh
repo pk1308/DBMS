7077efe4e7fbb59dd1d755409b8d1e6934dae9a2
pk1308 <princevkurien@gmail.com>
2024-05-09 14:41:38 +0400

    drivers added

--- driver/init_setup.sh
+++ driver/init_setup.sh
@@ -1,11 +1,9 @@
 echo [$(date)]: "START"
 export _VERSION_=3.11
-read _project_name 
-conda deacivate 
 echo [$(date)]: "creating environment with python ${_VERSION_}"
-conda create -n ${_project_name} python=${_VERSION_} -y
+conda create --prefix ./env python=${_VERSION_} -y
 echo [$(date)]: "activate environment"
-conda activate ${_project_name}
+conda activate ./env
 echo [$(date)]: "install requirements"
 pip3 install -r requirements.txt
 echo [$(date)]: "END"
